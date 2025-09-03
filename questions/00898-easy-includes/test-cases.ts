5eacebe04fdf28b19147707908921a9e3dd2e97e
dnlisc <54208946+dnlisc@users.noreply.github.com>
2025-09-03 15:37:28 +0300

    898 - Includes

--- questions/00898-easy-includes/test-cases.ts
+++ questions/00898-easy-includes/test-cases.ts
@@ -1,5 +1,7 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest] ? Equal<U, First> extends true ? true : Includes<Rest, U> : false
+
 type cases = [
   Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
   Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
