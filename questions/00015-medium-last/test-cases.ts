7354f595fa9133ac89d53a15974793c507058664
dnlisc <54208946+dnlisc@users.noreply.github.com>
2025-09-04 21:08:12 +0300

    15 - Last of Array

--- questions/00015-medium-last/test-cases.ts
+++ questions/00015-medium-last/test-cases.ts
@@ -1,5 +1,9 @@
 import type { Equal, Expect } from '@type-challenges/utils'
 
+type Last<T extends any[]> = T extends [] ? never : T['length'] extends 1 ? T[0] : T extends [infer _, ...infer Rest] ? Last<Rest> : never
+
+type Last2<T extends any[]> = T extends [...infer _, infer R] ? R : never
+
 type cases = [
   Expect<Equal<Last<[]>, never>>,
   Expect<Equal<Last<[2]>, 2>>,
